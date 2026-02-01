49bb720a221b6f68a0f73eaa4d5a67af1ebb3818
ayanadak68667-del <ayanadak68667@gmail.com>
2026-02-01 07:40:46 +0530

    Update HomeCache.js

--- models/HomeCache.js
+++ models/HomeCache.js
@@ -1,1 +1,19 @@
+const mongoose = require("mongoose");
 
+const homeCacheSchema = new mongoose.Schema({
+  key: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  data: {
+    type: Object,
+    required: true
+  },
+  lastUpdated: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("HomeCache", homeCacheSchema);
